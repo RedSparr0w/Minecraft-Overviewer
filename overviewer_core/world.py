2c168d3da2ca89476c115c5e71e0a0f6b9e34790
Danial Nickford <admin@redsparr0w.com>
2018-09-17 09:42:04 +1200

    rearrange

--- overviewer_core/world.py
+++ overviewer_core/world.py
@@ -730,12 +730,12 @@ class RegionSet(object):
                 'light_gray',   'cyan',  'purple',       'blue',
                      'brown',  'green',     'red',      'black']
         for i in range(len(colors)):
-            self._blockmap['minecraft:%s_stained_glass_pane' % colors[i]] = (160, i)
-            self._blockmap['minecraft:%s_concrete_powder'    % colors[i]] = (252, i)
             self._blockmap['minecraft:%s_stained_glass'      % colors[i]] = (95, i)
+            self._blockmap['minecraft:%s_stained_glass_pane' % colors[i]] = (160, i)
+            self._blockmap['minecraft:%s_banner'             % colors[i]] = (176, i)
+            self._blockmap['minecraft:%s_wall_banner'        % colors[i]] = (177, i)
             self._blockmap['minecraft:%s_concrete'           % colors[i]] = (251, i)
-            self._blockmap['minecraft:%s_banner'           % colors[i]] = (176, i)
-            self._blockmap['minecraft:%s_wall_banner'           % colors[i]] = (177, i)
+            self._blockmap['minecraft:%s_concrete_powder'    % colors[i]] = (252, i)
 
 
     # Re-initialize upon unpickling
