88cff1ec7a1690dcec937fbae6064726476d979e
RedSparr0w <admin@redsparr0w.com>
2018-09-19 21:26:06 +1200

    add some top slabs

--- overviewer_core/world.py
+++ overviewer_core/world.py
@@ -745,6 +745,9 @@ class RegionSet(object):
                 data |= 0x08
         elif key == 'minecraft_wheat':
             data = int(palette_entry['Properties']['age'])
+        elif key in ['minecraft:stone_slab', 'minecraft:sandstone_slab', 'minecraft:oak_slab', 'minecraft:cobblestone_slab', 'minecraft:brick_slab', 'minecraft:stone_brick_slab', 'minecraft:nether_brick_slab', 'minecraft:quartz_slab']:
+            if palette_entry['Properties']['type'] == 'top':
+                data += 8
         elif key in ['minecraft:ladder', 'minecraft:chest']:
             facing = palette_entry['Properties']['facing']
             data = {'north': 2, 'south': 3, 'west': 4, 'east': 5}[facing]
